f013438e3a81edb12a5527a8051794b60596c52d
EC2 Default User <ec2-user@ip-172-31-87-60.ec2.internal>
2019-11-08 08:22:37 +0000

    added hft case

---
+++ hft/kirin.cpp
@@ -0,0 +1,51 @@
+#define MMBot Codename1
+#define RandomTakerBot Codename2
+#define CreepBot Codename3
+
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <deque>
+#include <queue>
+#include <mutex>
+#include <thread>
+#include <random>
+
+
+
+#include "bot.hpp"
+#include "client_state.hpp"
+#include "common.hpp"
+#include "creep_bot.hpp"
+#include "exchange.hpp"
+#include "manager.hpp"
+#include "mmbot.hpp"
+#include "order_book.hpp"
+#include "random_taker_bot.hpp"
+#include "receiver_exchange.hpp"
+#include "receiver_client.hpp"
+#include "sender_exchange.hpp"
+#include "sender_client.hpp"
+#include "thread_safe_queue.hpp"
+#include "process_queue.hpp"
+#include "queue_manager.hpp"
+#include "trader.hpp"
+
+#include "bot.cpp"
+#include "client_state.cpp"
+#include "exchange.cpp"
+#include "manager.cpp"
+#include "order_book.cpp"
+#include "receiver_exchange.cpp"
+#include "receiver_client.cpp"
+#include "sender_exchange.cpp"
+#include "thread_safe_queue.cpp"
+#include "queue_manager.cpp"
+#include "trader.cpp"
